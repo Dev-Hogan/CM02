a550b3a6c8ac2f7718db9a502cb1b55743988341
Hutao <mm_Huang_work@163.com>
2023-06-02 09:44:01 +0800

    登录背景图

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,18 @@
 <template>
-	<div>
-        登录
-    </div>
+	<div class="login-container">
+		登录
+		<div class="login-form"></div>
+	</div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
+.login-container {
+    min-height: 100vh;
+	background-image: url(../assets/login-1.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+}
 
 </style>
